f6ff816391ae4c8612def06c6e7e92ba5942c130
ALLAN MACHADO GONÇALVES <112505766+zAllaNz@users.noreply.github.com>
2023-02-02 23:06:42 -0300

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(lista):
+    for j in range(len(lista)):
+        if(lista[0] > lista[j]):
+            aux = lista[0]
+            lista[0] = lista[j]
+            lista[j] = aux
+    for j in range(2,len(lista)):
+        aux = lista[j]
+        i = j - 1
+        while(i>0 and lista[i] > aux):
+            lista[i+1] = lista[i]
+            i = i - 1
+        lista[i+1] = aux
+
+teste = [5,3,4,2,7,8,9,12,5,3,1,5,8,4,7,6,3,1,5,8,9]
+print("Antes:", teste)
+insertion_sort(teste)
+print("Resultado:", teste)
